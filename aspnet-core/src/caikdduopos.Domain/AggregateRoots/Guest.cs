0b1c4c12dafac3ed36d02cac5cbf36f232d0d142
mattcoder2020 <mattcoder2013@yahoo.com>
2023-08-30 21:23:04 +0800

    Added the poweshell script for docker

---
+++ aspnet-core/src/caikdduopos.Domain/AggregateRoots/Guest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace caikdduopos.AggregateRoots
+{
+    public class Guest: AuditedAggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ installupgrade.ps1
@@ -0,0 +1,4 @@
+docker-compose -f docker-compose-3party.yml build
+docker-compose -f docker-compose-3party.yml up
+
+
