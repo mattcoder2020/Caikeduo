00f9ba83ba62012a46aa7d4e5147dd139a72b28c
mattcoder2017 <myang@arris.com>
2024-02-13 09:11:45 +0800

    centos adjustment

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,18 +1,18 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'https://localhost:44357';
+const baseUrl = 'http://0.0.0.0:44357';
 
 export const environment = {
   production: false,
   application: {
     baseUrl,
-    name: 'Caikeduo',
+    name: 'AccessManagment',
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'https://localhost:44357/',
+    issuer: 'https://0.0.0.0:44357/',
     redirectUri: baseUrl,
-    clientId: 'Caikeduo',
+    clientId: 'AccessManagment_Web',
     responseType: 'code',
     scope: 'offline_access AccessManagment',
     requireHttps: true,
