d9b6ccc68252c1a252163aa222dbac086b8e4d9c
mattcoder2017 <myang@arris.com>
2024-02-13 11:08:47 +0800

    centos

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'https://119.23.216.60:44357/';
+const baseUrl = 'http://localhost:4200';
 
 export const environment = {
   production: false,
@@ -12,14 +12,14 @@ export const environment = {
   oAuthConfig: {
     issuer: 'https://119.23.216.60:44357/',
     redirectUri: baseUrl,
-    clientId: 'AccessManagment_Web',
+    clientId: 'AccessManagment_App',
     responseType: 'code',
     scope: 'offline_access AccessManagment',
     requireHttps: true,
   },
   apis: {
     default: {
-      url: 'https://119.23.216.60:44357/',
+      url: 'https://119.23.216.60:44357',
       rootNamespace: 'AbpSampleApp.AccessManagment',
     },
   },
