088c9f5908502ac2fecc683e50dd140811d8cf88
mattcoder2017 <myang@arris.com>
2024-02-13 12:14:58 +0800

    cntos

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://localhost:4200';
+const baseUrl = 'http://119.23.216.60:4200';
 
 export const environment = {
   production: false,
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://localhost:5200';
+const baseUrl = 'http://localhost:4200';
 
 export const environment = {
   production: false,
