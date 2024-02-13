01c807909504dadb7ec1ec9dc2bdbb87271edc40
mattcoder2017 <myang@arris.com>
2024-02-13 09:37:32 +0800

    centos deployment

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'http://0.0.0.0:44357';
+const baseUrl = 'http://localhost:44357';
 
 export const environment = {
   production: false,
@@ -10,7 +10,7 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'https://0.0.0.0:44357/',
+    issuer: 'https://localhost:44357/',
     redirectUri: baseUrl,
     clientId: 'AccessManagment_Web',
     responseType: 'code',
